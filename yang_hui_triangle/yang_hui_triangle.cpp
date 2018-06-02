2288458d841e65c0d6770d40aba03176ddf6575e
Pheobeljj <liujj0825@163.com>
2018-06-02 21:48:56 +0800

    添加项目文件。

---
+++ yang_hui_triangle/yang_hui_triangle.cpp
@@ -0,0 +1,37 @@
+// yang_hui_triangle.cpp : 定义控制台应用程序的入口点。
+//
+
+#include "stdafx.h"
+
+
+int main()
+{
+	int a[11][20], i, j;
+	//初始化 
+	for (i = 0; i<11; i++)
+		for (j = 0; j<20; j++)
+			a[i][j] = 0;
+	//给数组附值 
+	for (i = 0; i<10; i++)
+	{
+		for (j = 0; j <= i; j++)
+		{
+			if (j<1)a[i][j] = 1;//开头的第一个数为1 
+			else if (i == 0)break;
+			else
+				a[i][j] = a[i - 1][j - 1] + a[i - 1][j];
+			//杨辉三角的规律 
+		}
+	}
+	//输出 
+	for (i = 0; i<10; i++)
+	{
+		for (j = 0; j <= i; j++)
+			printf("%d ", a[i][j]);
+		printf("\n");
+	}
+	getchar();
+	getchar();
+	return 0;
+}
+
